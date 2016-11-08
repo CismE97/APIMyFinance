f019f43a08769386019134b2b9ec7e51779a8f71
Christe Thibaud <thibaudchriste1997@gmail.com>
2016-11-08 17:42:41 +0100

    Adaptation pour la mise en ligne de l'API de connexion

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 $user = $request->user;
 $password = $request->password;
 
 
-if($user == 'test'){
+if($user == 'test' && $password == '1234'){
   echo '1';
 }else{
   echo '0';
